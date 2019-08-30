2fe5d751daff254bf550a7ab1e81c8e76a8e7fa2
Administrator <you@example.com>
2019-08-30 16:33:41 +0530

    Updates

---
+++ precache-manifest.539ca149e86d1bb41c052207d620fe36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2483bc08a05da78973d34ffe4b9acd93",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "de176bc06e9f44384855",
+    "url": "/myapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "14cdadbbf1da29303298",
+    "url": "/myapp/static/js/2.e8d5f8f8.chunk.js"
+  },
+  {
+    "revision": "de176bc06e9f44384855",
+    "url": "/myapp/static/js/main.f985418b.chunk.js"
+  },
+  {
+    "revision": "37e3cfc9c62b4b1b50ca",
+    "url": "/myapp/static/js/runtime~main.80f2ff90.js"
+  }
+]);
