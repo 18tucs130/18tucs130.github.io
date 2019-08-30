4d80a2dd5d7830c7f10d5928c11351e9599f60c9
Administrator <you@example.com>
2019-08-30 16:28:46 +0530

    intial commit

---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Mainpage extends React.Component{
+        render(){
+                //overrides render inside React.Component
+		return(
+			<div>
+      	 		   <p>Name</p>
+                           <p>college name</p>
+                       </div>
+			)
+		}
+}
+
